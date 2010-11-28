d5cc77b2600a773ae9ed1ffd8e0a83644ac0ab36
Brandon R. Stoner <monokrome@monokro.me>
2010-11-28 01:27:48 -0700

    Removed a comment in which I've got no clue regarding it's existance in the first place.

--- modules/skins/config/skins.php
+++ modules/skins/config/skins.php
@@ -1,7 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-		 // By default, use the Apache XSendfile action
 	'default_skin' => 'default',
 	'skin_path'    => 'skins',
 );
